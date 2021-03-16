64bd6467b3ff74c422b1e5e055061b142fc9e9c6
koroko03 <korokonamae@gmail.com>
2021-03-16 13:14:09 +0700

    Tugas Class dan Objek Taksi

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taksi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Membuat Objek Taksi
+            Taksi taxi = new Taksi();
+
+            // Setting nilai properties
+            taxi.DriverName = "Jati Negara";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            // Pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taksi.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taksi
+{
+    public class Taksi
+    {
+        // Properties
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        // Method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", DriverName);
+            if (OnDuty == true)
+            {
+                Console.WriteLine("On Duty : Yes");
+            }
+            else if (OnDuty == false)
+            {
+                Console.WriteLine("On Duty : No");
+            }
+            Console.WriteLine("Number of Passenger : {0}", NumPassenger);
+            Console.WriteLine("\n");
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", DriverName);
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang\n", DriverName);
+        }
+    }
+}
